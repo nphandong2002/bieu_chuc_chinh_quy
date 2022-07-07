39822288487c47d5fa1e281dcfa541897fbc01fc
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-07-07 12:05:43 +0700

    new

--- src/program.java
+++ src/program.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class program {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class program {
+
+    private final  static String   reg =  "^B";
+    private  final static String text = "B2345";
+    private static  Pattern pattern;
+    private  static Matcher matcher;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean match = true;
+        pattern = Pattern.compile(reg);
+
+        System.out.print("Mã SInh viên:  ");
+        String text   = input.nextLine();
+
+        match = pattern.matcher(text).lookingAt();
+        if (match) {
+            text = pattern.matcher(text).replaceFirst("");
+            try{
+                int sau = Integer.parseInt(text.trim());
+                System.out.println("Thành công");
+            }catch (Exception ex) {
+                System.out.println(ex);
+                System.out.println("Mã sinh viên có dạng Bxxxxxxx với x là 1 số nguyên từ 1 đến 9");
+            }
+
+        } else {
+            System.out.println("Chữ cái đầu phải là B");
+        }
+    }
 }
